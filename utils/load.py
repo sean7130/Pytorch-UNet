fbea99747b74de1a8f385bd37d4c034c0323fd3c
Yi Jing Tong <sean.tong@mail.utoronto.ca>
2019-11-01 10:55:13 -0400

    split up lines in loader

--- utils/load.py
+++ utils/load.py
@@ -17,7 +17,7 @@ def to_cropped_imgs(ids, dir, suffix, scale):
     """From a list of tuples, returns the correct cropped img"""
     for id in ids:
         # print(suffix)
-        if suffix == '_mask.gif' and (dir == "data/masks/" or dir == "data_cat/masks/" or dir == "cat_data_aug/masks/"):
+        if suffix == '_mask.gif' and (dir == "data/masks/" or dir == "data_cat/masks/"):
           # Case of using cat data masks
           # filename = "masks_"+id+".jpg"
           img_name_adjusted = dir + "mask_" + id + ".jpg"
@@ -25,7 +25,11 @@ def to_cropped_imgs(ids, dir, suffix, scale):
           im = resize_and_crop(Image.open(img_name_adjusted), scale=scale)
           im = im/255
           yield im
-
+        elif suffix == "_mask.gif" and dir == "cat_data_aug/masks/":
+          img_name_adjusted = dir + "mask_" + id + ".jpg"
+          im = resize_and_crop(Image.open(img_name_adjusted), scale=scale)
+          im = im/255
+          yield im 
         else:
           # any other case, for example using car data
           # filename = id+suffix, suffix is "_masks.jpg"
